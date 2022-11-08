8efb762dbef0c3d829f48079b7589d84c3058045
ShTAdi <32208430+ShTAdi@users.noreply.github.com>
2022-11-09 01:42:33 +0300

    Add files via upload
    Тестовое задание Калькулятор (по результатам замечаний Ментора)

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,22 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculator {
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        Scanner r = new Scanner(System.in);
+        String str = r.nextLine();
+        try {
+            calc(str);
+                System.out.printf(calc(str));
+        }catch (Exception e){
+                System.out.println("Exception thrown");
+        }
+    }
+    public static String calc (String input) throws IOException{
+        try {//-------------------------------------------основной Try------------------------------
+            String[] strArr = new String[0];
             String[] strR = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
             String[] strR2 = {
-                    "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX","X",
+                    "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
                     "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", // 20
                     "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", // 30
                     "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXII", "XXXIII", "XXXIX", "XL", // 40
@@ -16,104 +28,93 @@ public class Calculator {
                     "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", // 90
                     "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"// 100
             };
+            strArr = input.split(" ", 3);
+            byte num1 = 0;
+            byte num2 = 0;
             String[] strROp = {"+", "-", "*", "/"};
-            String[] strArr;
             String str3;
-
             char op;
-            Scanner r = new Scanner(System.in);
-            if (r.hasNextByte()) {
-                byte num1;
-                byte num2;
-                try {
-                    String str = r.nextLine();
-                    strArr = str.split(" ", 3);
-                    num1 = Byte.parseByte(strArr[0]);
-                    num2 = Byte.parseByte(strArr[2]);
-                    if ((num1 >= 0 && num1 <= 10) && (num2 >= 0 && num2 <= 10)) {
+            if (Character.isDigit(input.charAt(0))) {//---------------if up----------------------------------
+                num1 = Byte.parseByte(strArr[0]);
+                num2 = Byte.parseByte(strArr[2]);
+                if ((num1 >= 0 && num1 <= 10) && (num2 >= 0 && num2 <= 10)) {
+                } else {
+                    throw new Exception();
+                }
+                str3 = strArr[1];
+                for (int i = 0; true; i++) {
+                    if (str3.equals(strROp[i])) {
+                        break;
+                    }
+                }
+                op = (char) strArr[1].charAt(0);
+                if ((op == '-' || op == '+') || (op == '/' || op == '*')) {
+                    if (op == '+') {
+                        input = Integer.toString(num1 + num2);
+                    } else if (op == '-') {
+                        input = Integer.toString(num1 - num2);
+                    } else if (op == '*') {
+                        input = Integer.toString(num1 * num2);
                     } else {
-                        throw new Exception();
+                        input = Integer.toString(num1 / num2);
                     }
-                    str3 = strArr[1];
-                    for (int i = 0; true; i++) {
-                        if (str3.equals(strROp[i])) {
-                            break;
-                        }
-                    }
-                    op = (char) strArr[1].charAt(0);
-                    if ((op == '-' || op == '+') ||  (op == '/' || op == '*')){
-                        if (op == '+'){
-                            System.out.println(num1 + num2);
-                        }else if (op == '-'){
-                            System.out.println(num1 - num2);
-                        }else if (op == '*'){
-                            System.out.println(num1 * num2);
-                        }else {
-                            System.out.println(num1 / num2);
-                        }
-                    } else {
-                        throw new Exception();
-                    }
-                } catch (Exception e) {
-                    System.out.println("Exception thrown");
-                }//--------------------------------------------------------------------------------------------------------
-            }else {
+                } else {
+                    throw new Exception();
+                }
+                return input;
+            } else {//--------if end/ Else up-----------------------------------------------------------------------------------
                 String str1;
                 String str2;
-
                 int numStr1 = 0;
                 int numStr2 = 0;
                 int numStrRes;
-                String strRes = null;
-                try {
-                    String str = r.nextLine();
-                    strArr = str.split(" ", 3);
-                    str1 = strArr[0];
-                    for (int i = 0; true; i++) {
-                        if (str1.equals(strR[i])) {
-                            numStr1 = ++i;
-                            break;
-                        }
+                str1 = strArr[0];
+                for (int i = 0; true; i++) {
+                    if (str1.equals(strR[i])) {
+                        numStr1 = ++i;
+                        break;
                     }
-                    str2 = strArr[2];
-                    for (int i = 0; true; i++) {
-                        if (str2.equals(strR[i])) {
-                            numStr2 = ++i;
-                            break;
-                        }
-                    }
-                    str3 = strArr[1];
-                    for (int i = 0; true; i++) {
-                        if (str3.equals(strROp[i])) {
-                            break;
-                        }
-                    }
-                    op = (char) strArr[1].charAt(0);
-                    if ((op == '-' || op == '+') ||  (op == '/' || op == '*')) {
-                        if (op == '+'){
-                            numStrRes = (numStr1 + numStr2) - 1;
-                            strRes = strR[numStrRes];
-                            System.out.println(strRes);
-                        }else if (op == '-'){
-                            numStrRes = (numStr1 - numStr2) - 1;
-                            strRes = strR[numStrRes];
-                            System.out.println(strRes);
-                        }else if (op == '*'){
-                            numStrRes = (numStr1 * numStr2) - 1;
-                            strRes = strR2[numStrRes];
-                            System.out.println(strRes);
-                        }else {
-                            numStrRes = (numStr1 / numStr2) - 1;
-                            strRes = strR[numStrRes];
-                            System.out.println(strRes);
-                        }
-                    } else {
-                        throw new Exception();
-                    }
-                } catch (Exception e) {
-                    System.out.println("Exception thrown");
                 }
+                str2 = strArr[2];
+                for (int i = 0; true; i++) {
+                    if (str2.equals(strR[i])) {
+                        numStr2 = ++i;
+                        break;
+                    }
+                }
+                str3 = strArr[1];
+                for (int i = 0; true; i++) {
+                    if (str3.equals(strROp[i])) {
+                        break;
+                    }
+                }
+                op = (char) strArr[1].charAt(0);
+                if ((op == '-' || op == '+') || (op == '/' || op == '*')) {
+                    if (op == '+') {
+                        numStrRes = (numStr1 + numStr2) - 1;
+                        input = strR[numStrRes];
+                    } else if (op == '-') {
+                        numStrRes = (numStr1 - numStr2) - 1;
+                        input = strR[numStrRes];
+                    } else if (op == '*') {
+                        numStrRes = (numStr1 * numStr2) - 1;
+                        input = strR2[numStrRes];
+                    } else {
+                        numStrRes = (numStr1 / numStr2) - 1;
+                        input = strR[numStrRes];
+                    }
+                } else {
+                    throw new Exception();
+                }
+                return input;
+            }//-------------------------------Else end---------------------------------------
+        }catch (Exception e){
+            try {
+                throw e;
+            } catch (Exception ex) {
+                //throw new RuntimeException(ex);
+                throw new IOException(e);
             }
         }
+    }
 }
-
